f78e6033fe0e5b7034ad635cfdf7fee698afa0f0
Max Pugh <maxpugh@Maxs-MBP.lan>
2023-08-11 19:00:03 -0700

    adding purple branch

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
                                 </li>
                             </span>
                             <span className="hover:translate-x-1 duration-100">
-                                <li className=" p-2 rounded-s-xl bg-gradient-to-br from-green-400 to-green-100 text-gray-700 ml-8 cursor-pointer lg:p-4 lg:text-xl font-mulish">
+                                <li className=" p-2 rounded-s-xl bg-gradient-to-br from-purple-600 to-purple-100 text-gray-700 ml-8 cursor-pointer lg:p-4 lg:text-xl font-mulish">
                                     <a
                                         className=" "
                                         href="https://docs.google.com/document/d/1ltW_z3f44Tk6u_A94l-yRu90Wby3NKEd6uH4IcWg3Pc/edit?usp=sharing"
@@ -42,12 +42,12 @@ export default function Home() {
                             </span>
                         </ul>
                     </nav>
-                    <div className="text-center pt-10 sm:px-10 ">
-                        <h2 className="text-5xl py-2 text-green-400 font-medium md:text-6xl">
+                    <div className="text-left pt-10 sm:px-10">
+                        <h2 className="text-5xl py-2 text-purple-600 font-medium md:text-6xl">
                             Max Gulati Pugh
                         </h2>
                         <h3 className="text-2xl py-2 md:text-3xl">Software Engineer</h3>
-                        <p className="leading-10 md:leading-10 text-md py-5 md:text-xl sm:max-w-4xl mx-auto">
+                        <p className="leading-10 md:leading-10 text-md py-5 md:text-xl sm:max-w-4xl">
                             I'm a Los Angeles based software developer and musician
                             originally from Saint Louis, Missouri. My creative journey has
                             given me a deep appreciation for diverse perspectives,
@@ -78,13 +78,13 @@ export default function Home() {
                     <div className="max-w-6xl pt-4 px-xl m-auto ">
                         <h3 className="text-3xl py-2 font-mulish"> Portfolio </h3>
                         <p className="text-lg md:text-lg leading-8 gap-10 py-10 max-w-6xl mx-auto">
-                            From crafting <span className="text-green-500">interactive user interfaces</span> to building <span className="text-green-500">robust server-side logic</span>, I've got the full stack covered. Most recently, I've been using Java with <span className="text-green-500">Spring Boot</span> and <span className="text-green-500">AWS</span> to build <span className="text-green-500">RESTful APIs</span>. I'm also experienced with <span className="text-green-500">React</span> and <span className="text-green-500">Next.js</span> for front-end development. I'm always looking to learn new technologies and expand my skillset.
+                            From crafting <span className="text-purple-600">interactive user interfaces</span> to building <span className="text-purple-600">robust server-side logic</span>, I've got the full stack covered. Most recently, I've been using Java with <span className="text-purple-600">Spring Boot</span> and <span className="text-purple-600">AWS</span> to build <span className="text-purple-600">RESTful APIs</span>. I'm also experienced with <span className="text-purple-600">React</span> and <span className="text-purple-600">Next.js</span> for front-end development. I'm always looking to learn new technologies and expand my skillset.
                         </p>
 
                         <div className="basis-1/3 flex-1 py-5">
                             <div className="hover:translate-x-1 duration-100">
                                 <a href="https://github.com/mpughcs/Notation">
-                                    <BiLinkExternal className="text-2xl text-green-400 absolute translate-y-5 translate-x-4" />
+                                    <BiLinkExternal className="text-2xl text-purple-600 absolute translate-y-5 translate-x-4" />
                                 </a>
                             </div>
                            
@@ -100,7 +100,7 @@ export default function Home() {
                         <div className="basis-1/3 flex-1 py-5">
                             <div className="hover:translate-x-1 duration-100">
                                 <a href="https://sundaymourners.com/">
-                                    <BiLinkExternal className="text-2xl text-green-400 absolute translate-y-5 translate-x-4" />
+                                    <BiLinkExternal className="text-2xl text-purple-600 absolute translate-y-5 translate-x-4" />
                                 </a>
                             </div>
                             <Image
@@ -119,7 +119,7 @@ export default function Home() {
                             <div className="basis-1/3 flex-1 py-5">
                                 <div className="hover:translate-x-1 duration-100">
                                     <a href="https://github.com/mpughcs/Max_Pugh_Java/tree/main/Summative-2-Pugh-Max">
-                                        <BiLinkExternal className="text-2xl text-green-400 absolute translate-y-5 translate-x-4" />
+                                        <BiLinkExternal className="text-2xl text-purple-600 absolute translate-y-5 translate-x-4" />
                                     </a>
                                 </div>
                                 <Image
@@ -136,7 +136,7 @@ export default function Home() {
                             <div className="basis-1/3 flex-1 py-5">
                                 <div className="hover:translate-x-1 duration-100">
                                     <a href="https://github.com/mpughcs/Notation">
-                                        <BiLinkExternal className="text-2xl text-green-400 absolute translate-y-5 translate-x-4" />
+                                        <BiLinkExternal className="text-2xl text-purple-600 absolute translate-y-5 translate-x-4" />
                                     </a>
                                 </div>
                                 <Image
@@ -152,7 +152,7 @@ export default function Home() {
                         )}
                         <div className="flex justify-end hover:translate-x-1 duration-100">
                             <button
-                                className="p-2  rounded-s-xl left-full bg-gradient-to-br from-green-400 to-green-100 text-gray-700 ml-8 cursor-pointer lg:p-4 lg:text-xl font-mulish"
+                                className="p-2  rounded-s-xl left-full bg-gradient-to-br from-purple-600 to-purple-100 text-gray-700 ml-8 cursor-pointer lg:p-4 lg:text-xl font-mulish"
                                 onClick={() => setShowLastImages(!showLastImages)}
                             >
                                 {showLastImages ? "Less Project" : "More Projects"}
@@ -166,7 +166,7 @@ export default function Home() {
                             As I near the completion of my CS degree in the winter of 2023, 
                             I'm eager to put my skills to use with a forward thinking team of creative
                             developers. I'm currently looking for a full time software engineering position
-                             for January 2024. If you're interested in working together, lets connect! You can always reach me <span className="text-green-500"> <a href="mailto:maxgpugh01@gmail.com">here</a></span>
+                             for January 2024. If you're interested in working together, lets connect! You can always reach me <span className="text-purple-600"> <a href="mailto:maxgpugh01@gmail.com">here</a></span>
                             
                             
                         </p>
@@ -187,22 +187,22 @@ export default function Home() {
                                 I'm always learning new frameworks and languages, but here are
                                 a few of my favorites
                             </p>
-                            <h4 className="text-green-400"> Technolgies I use: </h4>
+                            <h4 className="text-purple-600"> Technolgies I use: </h4>
                             <div className=" gap-8 py-3 flex justify-evenly">
                                 <div className="">
-                                    <h3 className="text-green-400"> Frontend </h3>
+                                    <h3 className="text-purple-600"> Frontend </h3>
                                     <ul>
                                         <li> ReactJs </li> <li> Next.js </li> <li> Tailwind </li>
                                     </ul>
                                 </div>
                                 <div className="">
-                                    <h3 className="text-green-400"> Backend </h3>
+                                    <h3 className="text-purple-600"> Backend </h3>
                                     <ul>
                                         <li> Spring Boot </li> <li> Node.js </li> <li> MySql </li>
                                     </ul>
                                 </div>
                                 <div className="">
-                                    <h3 className="text-green-400"> Scripting </h3>
+                                    <h3 className="text-purple-600"> Scripting </h3>
                                     <ul>
                                         <li> Python </li> <li> C++ </li> <li> Java </li>
                                     </ul>
@@ -222,10 +222,10 @@ export default function Home() {
                                 Some of the design tools I use for web, print, and video. 
                                 
                             </p>
-                            <h4 className="text-green-400"> Technolgies I use: </h4>
+                            <h4 className="text-purple-600"> Technolgies I use: </h4>
                             <div className=" gap-8 py-3 flex justify-evenly">
                                 <div className="">
-                                    <h3 className="text-green-400"> Design </h3>
+                                    <h3 className="text-purple-600"> Design </h3>
                                     <ul>
                                         <li> Adobe Illustrator </li> <li> Adobe XD </li>
                                         <li> Adobe Premier </li>
@@ -245,10 +245,10 @@ export default function Home() {
                             <p className="py-2 max-w-xs mx-auto">
                                 Whether I'm writing code or scoring a film, I use these tools every day
                             </p>
-                            <h4 className="text-green-400"> Technolgies I use: </h4>
+                            <h4 className="text-purple-600"> Technolgies I use: </h4>
                             <div className=" gap-8 py-3 flex justify-evenly">
                                 <div className="">
-                                    <h3 className="text-green-400"> Misc </h3>
+                                    <h3 className="text-purple-600"> Misc </h3>
                                     <ul>
                                         <li> Logic Pro X </li> <li> Unity  </li> <li> Trello  </li>
                                     </ul>
